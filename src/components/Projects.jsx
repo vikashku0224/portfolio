2b26548f052eb74b9ac1b26c56dba0f831549d0f
vikash50348 <vikashkumar50348@gmail.com>
2026-01-12 13:12:45 +0530

    Updated portfolio content and fixed styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,17 +15,19 @@ function Projects() {
           Here are a few <span className="text-primary">projects </span>i've
           worked on recentely
         </h1>
-        <div className="card " style={{ width: "18rem", marginLeft: "50px" }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">UpLoaded Soon...</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card’s content.
-            </p>
-            <a href="#" className="btn bg-black text-white">
-              <i className="bi bi-github"></i> GitHub
-            </a>
+        <div className="d-flex justify-content-center mt-2">
+          <div className="card text-dark" style={{ width: "18rem" }}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Upload Soon...</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <a href="#" className="btn bg-black text-white">
+                <i className="bi bi-github"></i> GitHub
+              </a>
+            </div>
           </div>
         </div>
       </div>
